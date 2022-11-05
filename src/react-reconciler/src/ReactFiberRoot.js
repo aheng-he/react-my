0f0de7b46c405c759661644622becf8be949c07a
heyuanheng <heyuanheng@able-elec.com>
2022-11-05 14:06:22 +0800

    ReactDOMRoot

---
+++ src/react-reconciler/src/ReactFiberRoot.js
@@ -0,0 +1,9 @@
+function FiberRootNode(containerInfo) {
+    this.containerInfo = containerInfo;
+  }
+  
+
+export function createFiberRoot(containerInfo){
+    const root = new FiberRootNode(containerInfo)
+    return root
+}
